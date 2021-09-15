821eb49f33111bf9dffedc70456deb41c5cc64bc
YogeshYadav-devit <86092759+YogeshYadav-devit@users.noreply.github.com>
2021-09-15 11:34:44 +0530

    Update environment.prod.ts

--- InternsManagementSystem/src/environments/environment.prod.ts
+++ InternsManagementSystem/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url : 'https://crudcrud.com/api/b0e9bb294b7945cd923a223750502581/intern'
+  url : 'https://crudcrud.com/api/639f871f112d4ebaab399361f12c7e3e/intern'
 };
